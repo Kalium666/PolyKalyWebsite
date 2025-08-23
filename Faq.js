8faef30433d07dfdb6fb2975804c61ce2c36c652
Kalium666 <maxetan@outlook.com>
2025-08-23 23:15:34 +0800

    Delete Faq.js

--- Faq.js
+++
@@ -1,7 +0,0 @@
-
-  document.querySelectorAll(".faq-question").forEach(question => {
-    question.addEventListener("click", () => {
-      const item = question.closest(".faq-item"); // ✅ ensures parent is toggled
-      item.classList.toggle("active");
-    });
-  });
